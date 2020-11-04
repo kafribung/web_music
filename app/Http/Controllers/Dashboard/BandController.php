1764a516d51e4892266994efd2ec5dfad943af79
kafribung <60900117001@uin-alauddin.ac.id>
2020-11-04 18:55:54 +0800

    dashboard

---
+++ app/Http/Controllers/Dashboard/BandController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class BandController extends Controller
+{
+    // Read
+    public function index()
+    {
+        return 'sapi';
+    }
+
+    // Create
+    public function create()
+    {
+        //
+    }
+
+    // Store
+    public function store(Request $request)
+    {
+        //
+    }
+
+    // Show
+    public function show($id)
+    {
+        //
+    }
+
+    // Edit
+    public function edit($id)
+    {
+        //
+    }
+
+    // / Update
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    // Destroy
+    public function destroy($id)
+    {
+        //
+    }
+}
